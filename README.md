98613182ae8c8aa8ad28cc1f85bad386e9fe5c42
public-release <as-public-release@janestreet.com>
2026-05-15 15:52:47 +0100

    v0.18~preview.130.100+614

--- README.md
+++
@@ -1,2 +0,0 @@
-# nonempty_list_type
-Underlying type of `Base.Nonempty_list.t`.
---
+++ src/nonempty_list_type.ml
@@ -0,0 +1,9 @@
+module Nonempty_list = struct
+  (** List with at least one element. Defined to use existing list syntax, e.g.:
+      {v
+        let a = [ 1; 2; 3 ]
+        let b = 1 :: 2 :: 3 :: []
+      v}
+      Functions supporting this type can be found in [Base.Nonempty_list]. *)
+  type 'a t = ( :: ) of 'a * 'a list
+end
